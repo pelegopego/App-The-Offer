bf8114db57f8aa01fda7e2bed850238820e59761
Matheus <matheuspellegrinini@gmail.com>
2021-01-25 16:27:25 -0300

    Ajustada versão

--- TheOffer/lib/utils/drawer_homescreen.dart
+++ TheOffer/lib/utils/drawer_homescreen.dart
@@ -493,7 +493,7 @@ class _HomeDrawer extends State<HomeDrawer> {
                 height: 90,
               ),
               Text(
-                '1.1.5',
+                '1.1.6',
                 style: TextStyle(
                     color: Colors.principalTheOffer,
                     fontWeight: FontWeight.w300),
