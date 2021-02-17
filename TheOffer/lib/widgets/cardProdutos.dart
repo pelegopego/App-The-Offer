01c263357c30f6c58556089c68f460270106cd1c
Matheus <matheuspellegrinini@gmail.com>
2021-02-17 17:15:23 -0300

    Removido uses desnecessário

--- TheOffer/lib/widgets/cardProdutos.dart
+++ TheOffer/lib/widgets/cardProdutos.dart
@@ -1,4 +1,3 @@
-import 'package:intl/intl.dart';
 import 'package:theoffer/screens/sabores.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/widgets.dart';
